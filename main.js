f0f94141397ec629683c4ccd1a2fcadf126a8780
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-17 17:27:40 -0500

    Create object and store data for Featured Teachers Section

--- main.js
+++ main.js
@@ -39,3 +39,45 @@ window.addEventListener('resize', disapearEcsResize);
 menuLinks.forEach(link => {
     link.addEventListener('click', disapear);
 });
+
+
+// Featured Teachers Section
+
+const teachersData = [
+    {
+        image: 'images/professors/prof1.svg',
+        name: 'Jhon Bleckler',
+        degree: 'Professor at Hardvard School',
+        description: 'Blenckler studies common-based peer production, and published his seminal book. The Wealth of Network in 2006.',
+    },
+    {
+        image: 'images/professors/prof2.svg',
+        name: 'Wilman Song',
+        degree: '',
+        description: 'Wilman Song bring the internet to Asia and is an outspoken advocate for the open web and digital commons. In 2012, he was included into the inagural class of the Internet Society\'s (ISOC) Internet Hall of Fame.',
+    },
+    {
+        image: 'images/professors/prof3.svg',
+        name: 'Teah Wuang',
+        degree: 'Director of Devweb Center and board member of CC France',
+        description: 'Teah Wuang Director of Devweb Center and board member of CC France As the main venue for new media art production in France, She promotes cross-disciplinary and understanding among science technology, humanities and the arts.',
+    },
+    {
+        image: 'images/professors/prof4.svg',
+        name: 'Lida Chase',
+        degree: 'President of Young Pirates of Asia',
+        description: 'Asian integration, political democracy and participation of youth thruogh online as her major condem. Reada\'s report outlining potential changes to EU copyright law was approved by the Parlament in July.',
+    },
+    {        
+        image: 'images/professors/prof5.svg',
+        name: 'Lila Trevor',
+        degree: 'Exceutive Director of Wikimedia Foundation',
+        description: 'Lila es the executive Director of the Wikimedia foundation, the nonprofit organization that operates Wikipedia is freely available in 200 languages and used by nearly half a billion people around the world every month.',
+    },
+    {        
+        image: 'images/professors/prof6.svg',
+        name: 'Ryan Jhonson',
+        degree: 'CEO of creative commons, ex COO of Mozilla Foundation',
+        description: 'He has been leading open-source projects at the Mozilla Foundation such as the open source movement.',
+    },
+];
